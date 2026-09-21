e17bd9c50f74419ce637986b934efde8c076d0b7
DekDanD <dekanadze2103@mail.ru>
2026-09-21 15:55:23 +0500

    add files

---
+++ ThreadSafeCache/ThreadSafeCache.cpp
@@ -0,0 +1,23 @@
+﻿#include "ThreadSafeCache.h"
+#include <iostream>
+#include <unordered_map>
+#include <chrono>
+using namespace std;
+
+template <typename Key, typename Value>
+inline shared_ptr<Value> ThreadSafeCache<Key, Value>::get(const Key& key)
+{
+	return nullptr;
+}
+
+template <typename Key, typename Value>
+inline void ThreadSafeCache<Key, Value>::remove(const Key& key)
+{
+
+}
+
+template <typename Key, typename Value>
+inline void ThreadSafeCache<Key, Value>::set(const Key& key, Value value, int ttl_ms)
+{
+
+}
---
+++ ThreadSafeCache/ThreadSafeCache.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <iostream>
+#include <unordered_map>
+#include <chrono>
+template <typename Key, typename Value>
+class ThreadSafeCache
+{
+	struct Item {
+		Value value;
+		std::chrono::steady_clock::time_point time;
+	};
+	std::unordered_map<Key, Item> cache;
+	int max_size;
+public:
+	void set(const Key& key, Value value, int ttl_ms);
+	std::shared_ptr<Value> get(const Key& key);
+	void remove(const Key& key);
+};
