0248656db96bbaf92f41b9f9338a7bae733306f9
DekDanD <dekanadze2103@mail.ru>
2026-09-22 15:39:00 +0500

    редактирование функций и добавление вещей для потокобезопасности и lru

--- ThreadSafeCache/ThreadSafeCache.cpp
+++ ThreadSafeCache/ThreadSafeCache.cpp
@@ -6,14 +6,19 @@
 template <typename Key, typename Value>
 inline std::shared_ptr<Value> ThreadSafeCache<Key, Value>::get(const Key& key)
 {
+	std::lock_guard<std::mutex> lock(this->mtx);
 	auto finding = this->cache.find(key);
 	if (finding != this->cache.end())
-		if (finding->second.time <= std::chrono::steady_clock::now())
+		if (finding->second->second.time <= std::chrono::steady_clock::now())
+		{
+			this->cache_list.erase(finding->second);
 			this->cache.erase(finding);
+		}
 		else
 		{
-			finding->second.time = std::chrono::steady_clock::now() + std::chrono::milliseconds(finding->second.ttl);
-			return std::make_shared<Value>(finding->second.value);
+			finding->second->second.time = std::chrono::steady_clock::now() + std::chrono::milliseconds(finding->second->second.ttl);
+			this->cache_list.splice(this->cache_list.end(), this->cache_list, finding->second);
+			return finding->second->second.value;
 		}
 	return nullptr;
 }
@@ -21,17 +26,39 @@ inline std::shared_ptr<Value> ThreadSafeCache<Key, Value>::get(const Key& key)
 template <typename Key, typename Value>
 inline void ThreadSafeCache<Key, Value>::remove(const Key& key)
 {
-	this->cache.erase(key);
+	std::lock_guard<std::mutex> lock(this->mtx);
+	auto finding = this->cache.find(key);
+	if (finding == this->cache.end())
+		return;
+	this->cache_list.erase(finding->second);
+	this->cache.erase(finding);
 }
 
 template <typename Key, typename Value>
 inline void ThreadSafeCache<Key, Value>::set(const Key& key, Value value, int ttl_ms)
 {
-	Item item{ value, ttl_ms, std::chrono::steady_clock::now() + std::chrono::milliseconds(ttl_ms) };
-	this->cache[key] = item;
+	std::lock_guard<std::mutex> lock(this->mtx);
+	auto finding = this->cache.find(key);
+	Item item{ std::make_shared<Value>(value), ttl_ms, std::chrono::steady_clock::now() + std::chrono::milliseconds(ttl_ms) };
+	if (finding != this->cache.end())
+	{
+		finding->second->second = item;
+		this->cache_list.splice(this->cache_list.end(), this->cache_list, finding->second);
+	}
+	else
+	{
+		if (this->cache_list.size() == this->max_size)
+		{
+			this->cache.erase(this->cache_list.front().first);
+			this->cache_list.pop_front();
+		}
+		this->cache_list.push_back(std::make_pair(key, item));
+		this->cache[key] = std::prev(this->cache_list.end());
+	}
+
 }
 
-int main() 
+int main()
 {
 
 }
--- ThreadSafeCache/ThreadSafeCache.h
+++ ThreadSafeCache/ThreadSafeCache.h
@@ -2,18 +2,23 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <utility>
+#include <mutex>
 #include <chrono>
 template <typename Key, typename Value>
 class ThreadSafeCache
 {
 	struct Item {
-		Value value;
+		std::shared_ptr<Value> value;
 		int ttl;
 		std::chrono::steady_clock::time_point time;
 	};
-	std::unordered_map<Key, Item> cache;
-	int max_size;
+	std::list<std::pair<Key, Item>> cache_list;
+	std::unordered_map<Key, typename std::list<std::pair<Key, Item>>::iterator> cache;
+	size_t max_size;
+	mutable std::mutex mtx;
 public:
+	ThreadSafeCache(size_t capacity) : max_size(capacity) {};
 	void set(const Key& key, Value value, int ttl_ms);
 	std::shared_ptr<Value> get(const Key& key);
 	void remove(const Key& key);
