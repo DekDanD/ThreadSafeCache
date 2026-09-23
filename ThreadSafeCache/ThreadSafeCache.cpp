b0ea6dedbdc88c596add25dc10fd8a5b533e205e
DekDanD <146200996+DekDanD@users.noreply.github.com>
2026-09-23 21:13:22 +0500

    Update ThreadSafeCache.cpp

--- ThreadSafeCache/ThreadSafeCache.cpp
+++ ThreadSafeCache/ThreadSafeCache.cpp
@@ -7,26 +7,31 @@
 template <typename Key, typename Value>
 inline std::shared_ptr<Value> ThreadSafeCache<Key, Value>::get(const Key& key)
 {
+	if (this->max_size == 0) return nullptr;
 	std::lock_guard<std::mutex> lock(this->mtx);
 	auto finding = this->cache.find(key);
 	if (finding != this->cache.end())
-		if (finding->second->second.time <= std::chrono::steady_clock::now())
+	{
+		auto iterator = finding->second;
+		if (iterator->second.time <= std::chrono::steady_clock::now())
 		{
-			this->cache_list.erase(finding->second);
 			this->cache.erase(finding);
+			this->cache_list.erase(iterator);
 		}
 		else
 		{
-			finding->second->second.time = std::chrono::steady_clock::now() + std::chrono::milliseconds(finding->second->second.ttl);
-			this->cache_list.splice(this->cache_list.end(), this->cache_list, finding->second);
-			return finding->second->second.value;
+			iterator->second.time = std::chrono::steady_clock::now() + std::chrono::milliseconds(iterator->second.ttl);
+			this->cache_list.splice(this->cache_list.end(), this->cache_list, iterator);
+			return iterator->second.value;
 		}
+	}
 	return nullptr;
 }
 
 template <typename Key, typename Value>
 inline void ThreadSafeCache<Key, Value>::remove(const Key& key)
 {
+	if (this->max_size == 0) return;
 	std::lock_guard<std::mutex> lock(this->mtx);
 	auto finding = this->cache.find(key);
 	if (finding == this->cache.end())
@@ -38,13 +43,15 @@ inline void ThreadSafeCache<Key, Value>::remove(const Key& key)
 template <typename Key, typename Value>
 inline void ThreadSafeCache<Key, Value>::set(const Key& key, Value value, int ttl_ms)
 {
+	if (this->max_size == 0) return;
 	std::lock_guard<std::mutex> lock(this->mtx);
 	auto finding = this->cache.find(key);
-	Item item{ std::make_shared<Value>(value), ttl_ms, std::chrono::steady_clock::now() + std::chrono::milliseconds(ttl_ms) };
+	Item item{ std::make_shared<Value>(std::move(value)), ttl_ms, std::chrono::steady_clock::now() + std::chrono::milliseconds(ttl_ms) };
 	if (finding != this->cache.end())
 	{
-		finding->second->second = item;
-		this->cache_list.splice(this->cache_list.end(), this->cache_list, finding->second);
+		auto iterator = finding->second;
+		iterator->second = std::move(item);
+		this->cache_list.splice(this->cache_list.end(), this->cache_list, iterator);
 	}
 	else
 	{
@@ -53,12 +60,10 @@ inline void ThreadSafeCache<Key, Value>::set(const Key& key, Value value, int tt
 			this->cache.erase(this->cache_list.front().first);
 			this->cache_list.pop_front();
 		}
-		this->cache_list.push_back(std::make_pair(key, item));
+		this->cache_list.push_back(std::make_pair(key, std::move(item)));
 		this->cache[key] = std::prev(this->cache_list.end());
 	}
-
 }
-
 int main()
 {
 	testing::InitGoogleTest();
